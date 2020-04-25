d628346c8846a0d0f6011bf2e64f0a25411222ac
Dong Hur <dhur@college.harvard.edu>
2020-04-25 14:24:15 -0400

    rudimentary connection to backend

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
-
 # import modules
 from flask import Flask, jsonify, request, render_template
-
+from database_interface import test_post
 # import local modules
 #import database_interface as DB
 
@@ -10,26 +9,27 @@ from flask import Flask, jsonify, request, render_template
 # instatiate flask
 app = Flask(__name__)
 
-
 # handling post and get requests
 @app.route('/index', methods=["GET","POST"])
 def index():
-	if request.method == 'GET':
-		print("Index page")
+    if request.method == 'GET':
+        print("Index page")
         # this message will be displayed
-		return jsonify({ "Page": "Index page"})
-	else:
-		return 'POST request received'
+        return jsonify({ "Page": "Index page"})
+    else:
+        return 'POST request received'
 
 # rending a HTLM page . NOTE: the HTML files are in the template folder
 @app.route('/')
 def viewHTLM():
+    test_post()
     return render_template('page2.html')
 
 # send a message from the url bar and rendering a page
 @app.route('/<message>')
 def sendToHTLM(message=None):
     return render_template('page.html', input_message=message)
-        
 
-# app.run(debug=True)
+# comment out during deployment
+if __name__ == '__main__':
+    app.run(debug=True)
--- database_interface.py
+++ database_interface.py
@@ -1,3 +1,14 @@
-# module for interacting with the database
+import os
+import pyrebase
+from flask import Flask, render_template, make_response, request
+from config import config
 
-# post and get request firebsase
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+app = Flask(__name__)
+
+def test_post():
+	db.child("todo").push("Here is one entry")
+	todo = db.child("todo").get()
+	return todo.val()
